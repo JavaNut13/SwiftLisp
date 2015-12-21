0ec2781475421d72ab1a786688125b30b4a372ac
Will Richardson <william.hamish@gmail.com>
2015-12-21 17:15:33 +1300

    Initial Commit

---
+++ SwiftLisp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftLisp
+//
+//  Created by Will Richardson on 21/12/15.
+//  Copyright © 2015 JavaNut13. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
